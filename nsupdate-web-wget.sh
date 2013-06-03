ffc8a8ab5ad019befccf2e8fc809a074df8b4c6b
Pat <pat@patriccio.com>
2013-06-02 20:42:10 -0400

    Updated "client" files
    Updated the wget update file and deleted the telnet update file.

--- nsupdate-web-wget.sh
+++ nsupdate-web-wget.sh
@@ -4,17 +4,24 @@
 #
 # (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://22pf.org/
 # Licensed under the GNU GPL 2.0 or later.
+#
+# Forked and modified by Pat O'Brien
+# https://github.com/drsprite/nsupdate-web
+#
+######################################################################
+
 
 # Config
 WGET=/usr/bin/wget
 
-URL="http://skontorp.net/nsupdate-web/"
-HOSTNAME="hermelin.skontorp.net"
-TTL="30" # Seconds TTL
-KEY="xxx" # Remember to URL encode this!
+URL="http://yourdomain.com/nsupdate-web/"
+HOSTNAME="test"
 IP="" # Default
+# TTL and KEY are not needed currently as I have deprecated them in the index.php file. 
+#TTL="30" # Seconds TTL
+#KEY="xxx" # Remember to URL encode this!
 
 # Execute
-PARAMS="?hostname=$HOSTNAME&ttl=$TTL&key=$KEY&ip=$IP"
+PARAMS="?hostname=$HOSTNAME&ip=$IP"
 
 $WGET -q -O - $URL$PARAMS
